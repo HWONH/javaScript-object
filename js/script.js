ab0fa2e63458ebfca69059fdb586eeed66fb3670
HWONH <95hyun408@naver.com>
2022-03-28 18:55:47 +0900

    array object

--- js/script.js
+++ js/script.js
@@ -213,3 +213,106 @@ var $final_op_phone=$ch_op_phone.replace("-", "");
 console.log($final_op_phone); // 01046791234
 
 // <a href="tel:01012345678">전화걸기</a>
+
+// 배열(Array) 객체 : 많은 양의 데이터를 하나의 변수에 저장할 수 있음
+var $product_arr=["kakao_01.jpg", "카카오 스티커", "1,500원"];
+document.write("<img src='img/"+$product_arr[0]+"'>");
+document.write("<h3>"+$product_arr[1]+"</h3>");
+document.write("<p>"+$product_arr[2]+"</p>");
+
+// 배열 데이터를 이용한 요일 정보 표현하기
+var $cur_today=new Date();
+var $day=$cur_today.getDay(); // 일 0, 월 1, 화 2, 수 3, 목 4, 금 5, 토 6
+var $day_arr=["일", "월", "화", "수", "목", "금", "토"];
+document.write("오늘은 "+$day_arr[$day]+"요일 입니다.", "<br>");
+
+// 배열 데이터를 이용한 오늘 요일 이미지 보여주기
+var $img_arr=["sun", "mon", "tues", "wednes", "thurs", "fri", "satur"];
+
+document.write("<img src='img/"+$img_arr[$day]+"day.gif' alt='"+$day_arr[$day]+"요일'>", "<br>");
+
+// 런닝맨 멤버
+var $member=["유재석", "김종국", "이광수", "지석진", "송지효", "양세찬", "전소민"];
+document.write("이번 주 물폭탄 당첨자 : "+$member[1], "<br>");
+$member.push("강하늘"); // push(새로운 데이터) : 맨 마지막 인덱스에 새로운 데이터를 추가
+document.write($member, "<br>");
+$member.pop(); // pop() : 맨 마지막 인덱스 데이터를 삭제
+document.write($member, "<br>");
+
+$member.shift(); // shift() : 맨 첫번째 인덱스 데이터를 삭제
+document.write($member, "<br>");
+$member.unshift("김수로"); // unshift(새로운 데이터) : 맨 첫번째 자리 이전에 새로운 데이터를 추가(인덱스 번호가 변경됨)
+document.write($member, "<br>");
+
+// 배열 데이터의 개수 구하기
+document.write($member.length, "<br>"); // 7
+
+// 배열 내부에 또 다른 배열(2차 배열)
+var $talent=[
+    ["송강호", "JSA"],                  // 1차 배열 기준 : 0
+    ["강하늘", "동갑내기 과외하기"],    // 1차 배열 기준 : 1
+    ["이병헌", "광해"]                  // 1차 배열 기준 : 2
+];
+document.write($talent[1][1], "<br>"); // 동갑내기 과외하기
+document.write($talent[2][0], "<br>"); // 이병헌
+
+// 배열 내부에서 네이밍을 지정하여 데이터를 가져올 수 있는 객체 배열(이중 배열)
+var $name_arr=[
+    {$name:"로마의 휴일", $actor:"오드리햅번"}, // 1차 배열 기준 : 0
+    {$name:"슈퍼맨", $actor:"크리스토퍼리브"}   // 1차 배열 기준 : 1
+];
+document.write($name_arr[1].$actor, "<br>"); // 크리스토퍼리브
+document.write($name_arr[0].$name, "<br>"); // 로마의 휴일
+
+// 배열의 sort에 대한 기능
+var $fruit=["banana", "orange", "apple"];
+var $sort_01=$fruit.sort();
+document.write($sort_01, "<br>"); // abc순
+/* 
+$fruit.sort();
+document.write($fruit, "<br>");
+ */
+
+var $price=["10000", "5000", "7000"]; // 가격 내 쉼표(,)를 사용안됨
+var $sort_02=$price.sort(function(a, b){ // 각 배열의 데이터로 접근 할 수 있는 변수를 지정
+    return a-b; // 지정된 변수를 상호 비교하여 돌려주겠다는 의미(내림차순)
+});
+document.write($sort_02, "<br>"); // 낮은 가격순
+var $sort_high=$sort_02.reverse(); // 현재 배열의 순서를 역순으로 재정렬
+document.write($sort_high, "<br>"); // 높은 가격순
+
+/* 
+$price.sort(function(a, b){
+    return a-b;
+});
+document.write($price, "<br>");
+var $price_high=$price.reverse();
+document.write($price_high, "<br>");
+ */
+
+ // 배열을 활용한 상품 카테고리 페이지 구성하기
+
+ // 배열패턴 = ["이미지 파일", "상품 타이틀", "상품 내용", "상품 가격"]
+ var pd_arr=[
+    ["shop_01.jpg", "마마스앤파파스", "NEW 스너그 트레이완구 포함", "월 17,400원"],
+    ["shop_02.jpg", "알집매트", "알집 꾸러기소파", "월 7,500원"],
+    ["shop_03.jpg", "알집매트", "알집 네오소파", "월 8,300원"],
+    ["shop_04.jpg", "알집매트", "알집 핑크퐁소파", "월 9,100원"],
+    ["shop_01.jpg", "마마스앤파파스", "NEW 스너그 트레이완구 포함", "월 17,400원"],
+    ["shop_02.jpg", "알집매트", "알집 꾸러기소파", "월 7,500원"],
+    ["shop_03.jpg", "알집매트", "알집 네오소파", "월 8,300원"],
+    ["shop_04.jpg", "알집매트", "알집 핑크퐁소파", "월 9,100원"]
+ ];
+ document.write("<section id='pd_cover'>");
+ /* 
+<div class='box'>
+<img src='img/shop_01.jpg' alt=''>
+<h2>마마스앤파파스</h2>
+<p class='cont'>NEW 스너그 트레이완구 포함</p>
+<p class='price'>월 17,400원</p>
+</div>
+ */
+ for(i=0;i<pd_arr.length;i++){
+    document.write("<div class='box'><img src='img/"+pd_arr[i][0]+"' alt=''><h2>"+pd_arr[i][1]+"</h2><p class='cont'>"+pd_arr[i][2]+"</p><p class='price'>"+pd_arr[i][3]+"</p></div>");
+ }
+ document.write("</section>");
